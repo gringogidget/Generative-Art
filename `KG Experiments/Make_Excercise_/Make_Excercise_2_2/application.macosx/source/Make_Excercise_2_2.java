e6b35b80239b7bdcb32ce27cde13282b50fbc878
gringogidget <kristinagroeger@gmail.com>
2014-06-14 22:38:41 -0400

    New Sketches
    And folder reorganization

---
+++ `KG Experiments/Make_Excercise_/Make_Excercise_2_2/application.macosx/source/Make_Excercise_2_2.java
@@ -0,0 +1,37 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Make_Excercise_2_2 extends PApplet {
+
+public void setup() {
+    size(480, 120);
+    smooth();
+}
+public void draw() {
+    if (mousePressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--full-screen", "--bgcolor=#666666", "--stop-color=#cccccc", "Make_Excercise_2_2" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ `KG Experiments/Make_Excercise_/Make_Excercise_3_2/Make_Excercise_3_2.pde
@@ -0,0 +1,35 @@
+size(480, 120);
+
+point(240, 60);
+point(240, 61);
+point(240, 62);
+point(240, 63);
+point(240, 64);
+
+point(239, 64);
+point(238, 64);
+point(237, 64);
+point(236, 64);
+point(235, 64);
+
+point(235, 63);
+point(235, 62);
+point(235, 61);
+point(235, 60);
+
+point(234, 59);
+point(233, 58);
+point(233, 57);
+point(233, 56);
+point(234, 55);
+point(235, 54);
+point(236, 54);
+point(237, 54);
+point(238, 54);
+point(239, 54);
+point(240, 55);
+point(241, 56);
+point(241, 57);
+point(241, 58);
+point(240, 59);
+
